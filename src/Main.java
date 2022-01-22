12695d6dbd6070444e098a92fb4706027199b8b1
paula <paula.anna.wesolowska@gmail.com>
2022-01-22 20:38:58 +0100

    added fourth task

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Main {
 
@@ -20,8 +21,32 @@ public class Main {
            boolean isIndexCorrect
            white(!isIndexCorrect) {}
         */
+        boolean isIndexCorrect = true;
+        String tab[] = {"Jeden", "Dwa", "Trzy", "Cztery", "Pięć", "Sześć"};
+        Scanner scan = new Scanner(System.in);
 
+        do {
+            int i = -1;
+            try {
+                System.out.println("Podaj indeks tablicy [0-5], który ma zostać wyświetlony: ");
+                i = Integer.parseInt(scan.next());
+            } catch (NumberFormatException e) {
+                System.out.println("Błąd, wprowadzony znak nie jest cyfrą!");
+                //throw e;
+            }
 
+            if ((i >= 0) && (i < tab.length-1) ){
+                try {
+                    System.out.println(tab[i]);
+                    isIndexCorrect = false;
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Błąd Podałeś index: " + i + ", a dozwolone są od 0 do " + (tab.length - 1));
+                } finally {
+                    System.out.println("Koniec programu");
+                }
+            }
+
+        } while(isIndexCorrect);
 
         /* zad.2
         a) stworzyć dowolny przykład z NullPonterException,
@@ -29,5 +54,30 @@ public class Main {
         c) stworzyć dowolny przykład z 2 lub 3 blokami catch i z blokiem finally,
          */
 
-    }
-}
+        try {
+            String s = null;
+            s.toString();
+        } catch (NullPointerException e) {
+            System.out.println("Błąd nie można wywołać metody toString() na null");
+            throw e;
+        }
+        int a = 10;
+        int b = 0;
+            try {
+                int result = a / b;
+                System.out.println(result);
+            } catch(ArithmeticException e) {
+                System.out.println("Błąd, nie można dzielić przez zero");
+            }
+
+        int arrayy[] = new int[17];
+        try {
+          arrayy[15] = 34 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Wystąpił błąd Arithmetic Exception ");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Wystąpił błąd ArrayIndexOutOfBounds");
+        } finally {
+            System.out.println("Koniec");
+        }
+    }}
